5f6ab879dd6de98c17b83552ab758197c25679a7
sharm <you@example.com>
2023-07-29 21:36:33 +0530

    First commit

--- Dsa/src/LinkedList/Main.java
+++ Dsa/src/LinkedList/Main.java
@@ -1,6 +1,30 @@
 package LinkedList;
 
-public class Main {
+class Node{
+	int data;
+	Node next;
+	Node(int data){
+		this.data=data;
+	}
+	public static void traverse(Node n1) {
+		Node temp=n1;
+		while(temp!=null) {
+			System.out.println(temp.data);
+			temp=temp.next;
+		}
+	}
 	
+}
+public class Main {
+	public static void main(String[] args) {
+		Node n1=new Node(5);
+		Node n2=new Node(10);
+		Node n3=new Node(15);
+		Node n4=new Node(20);
+		n1.next=n2;
+		n2.next=n3;
+		n3.next=n4;
+		Node.traverse(n1);
+	}
 
 }
