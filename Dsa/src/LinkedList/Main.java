c730a41f17bfde57d3e6b01a58a2faa382ff2895
aakash250798 <aakash250798@gmail.com>
2023-07-30 01:07:14 +0530

    Commit

--- Dsa/src/LinkedList/Main.java
+++ Dsa/src/LinkedList/Main.java
@@ -1,31 +1,49 @@
 package LinkedList;
 
 class Node{
+	static Node head=null;
 	int data;
 	Node next;
 	Node(int data){
 		this.data=data;
 	}
-	public static void traverse(Node n1) {
-		Node temp=n1;
+	public static void traverse() {
+		Node temp=head;
 		while(temp!=null) {
 			System.out.println(temp.data);
 			temp=temp.next;
 		}
 	}
-	
+	public static void insertAtBegin(int data) {
+		Node newNode=new Node(data);
+		if(head==null) {
+			head=newNode;
+			return;
+		}
+		else {
+			newNode.next=head;
+			head=newNode;
+		}
+	}
+	public static void insertAtEnd(int data) {
+		Node newNode=new Node(data);
+		if(head==null) {
+			head= newNode;
+			return ;
+		}
+		Node temp=head;
+		while(temp.next!=null) {
+			temp=temp.next;
+		}
+		temp.next=newNode;
+	}
 }
 public class Main {
 	public static void main(String[] args) {
-		
-		Node n1=new Node(5);
-		Node n2=new Node(10);
-		Node n3=new Node(15);
-		Node n4=new Node(20);
-		n1.next=n2;
-		n2.next=n3;
-		n3.next=n4;
-		Node.traverse(n1);
+		Node.insertAtBegin(10);
+		Node.insertAtEnd(5);
+		Node.insertAtEnd(20);
+		Node.traverse(); 
 	}
 
 }
